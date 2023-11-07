803b5eeb0589daaebfd7fb2e3a59f3747c3ff2b8
necojr <necoengenheiro@gmail.com>
2023-11-07 10:49:08 -0300

    refatoração resumo relatorio detecção

--- entity/resumo_relatorio_deteccao.go
+++ entity/resumo_relatorio_deteccao.go
@@ -1,25 +1,49 @@
 package entity
 
+type ResumoRelatorioDeteccaoClasseTotal struct {
+	Classe *DeteccaoClasse
+	Total  uint
+}
+
+type ResumoRelatorioDeteccaoClasseArea struct {
+	Classe *DeteccaoClasse
+	Total  float32
+}
+
 type ResumoRelatorioDeteccao struct {
-	TotalAnalises        uint
-	TotalDeteccoes       uint
-	TotalArea            float32
-	TotalDeteccaoPorTipo map[DeteccaoClasse]uint
-	TotalDeteccaoPorArea map[DeteccaoClasse]float32
+	TotalAnalises  uint
+	TotalDeteccoes uint
+	TotalArea      float32
+	ClassesTotal   []*ResumoRelatorioDeteccaoClasseTotal
+	ClassesArea    []*ResumoRelatorioDeteccaoClasseArea
+}
+
+func NewResumoRelatorioDeteccaoClasseTotal(classe *DeteccaoClasse, total uint) *ResumoRelatorioDeteccaoClasseTotal {
+	return &ResumoRelatorioDeteccaoClasseTotal{
+		Classe: classe,
+		Total:  total,
+	}
+}
+
+func NewResumoRelatorioDeteccaoClasseArea(classe *DeteccaoClasse, total float32) *ResumoRelatorioDeteccaoClasseArea {
+	return &ResumoRelatorioDeteccaoClasseArea{
+		Classe: classe,
+		Total:  total,
+	}
 }
 
 func NewResumoRelatorioDeteccao(
 	totalAnalises uint,
 	totalDeteccoes uint,
 	totalArea float32,
-	totalTipoPorDeteccao map[DeteccaoClasse]uint,
-	totalAreaPorDeteccao map[DeteccaoClasse]float32,
+	classesTotal []*ResumoRelatorioDeteccaoClasseTotal,
+	classesArea []*ResumoRelatorioDeteccaoClasseArea,
 ) *ResumoRelatorioDeteccao {
 	return &ResumoRelatorioDeteccao{
-		TotalAnalises:        totalAnalises,
-		TotalDeteccoes:       totalDeteccoes,
-		TotalArea:            totalArea,
-		TotalDeteccaoPorTipo: totalTipoPorDeteccao,
-		TotalDeteccaoPorArea: totalAreaPorDeteccao,
+		TotalAnalises:  totalAnalises,
+		TotalDeteccoes: totalDeteccoes,
+		TotalArea:      totalArea,
+		ClassesTotal:   classesTotal,
+		ClassesArea:    classesArea,
 	}
 }
