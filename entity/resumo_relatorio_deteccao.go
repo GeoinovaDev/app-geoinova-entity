c2929a3d09cb0ccd30179c28a9662bfb62262dce
necojr <necoengenheiro@gmail.com>
2023-11-07 08:55:09 -0300

    - adicionado area total no resumo do relatorio de deteccao

--- entity/resumo_relatorio_deteccao.go
+++ entity/resumo_relatorio_deteccao.go
@@ -3,6 +3,7 @@ package entity
 type ResumoRelatorioDeteccao struct {
 	TotalAnalises        uint
 	TotalDeteccoes       uint
+	TotalArea            float32
 	TotalDeteccaoPorTipo map[DeteccaoClasse]uint
 	TotalDeteccaoPorArea map[DeteccaoClasse]float32
 }
@@ -10,12 +11,14 @@ type ResumoRelatorioDeteccao struct {
 func NewResumoRelatorioDeteccao(
 	totalAnalises uint,
 	totalDeteccoes uint,
+	totalArea float32,
 	totalTipoPorDeteccao map[DeteccaoClasse]uint,
 	totalAreaPorDeteccao map[DeteccaoClasse]float32,
 ) *ResumoRelatorioDeteccao {
 	return &ResumoRelatorioDeteccao{
 		TotalAnalises:        totalAnalises,
 		TotalDeteccoes:       totalDeteccoes,
+		TotalArea:            totalArea,
 		TotalDeteccaoPorTipo: totalTipoPorDeteccao,
 		TotalDeteccaoPorArea: totalAreaPorDeteccao,
 	}
