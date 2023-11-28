666d84df38d8c437b56744925f5ef4122befe150
necojr <necoengenheiro@gmail.com>
2023-11-28 14:17:50 -0300

    feat: adicionado usuario role

--- entity/usuario_dominio.go
+++ entity/usuario_dominio.go
@@ -1,5 +1,12 @@
 package entity
 
+const (
+	USUARIO_ROLE_ADMIN     = "admin"
+	USUARIO_ROLE_USER      = "user"
+	USUARIO_ROLE_MODERATOR = "moderador"
+	USUARIO_ROLE_TESTER    = "tester"
+)
+
 type UsuarioDominio struct {
 	Id      uint
 	Dominio string
